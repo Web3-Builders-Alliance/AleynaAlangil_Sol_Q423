26fce9ef65dd4ad4e5e9a0c4c3188c673ec8959a
aleynaalangill <aleyna@firstbatch.xyz>
2023-11-28 12:49:03 +0300

    prereq commit

--- ts/prereqs/enroll.ts
+++ ts/prereqs/enroll.ts
@@ -10,7 +10,7 @@ const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 const connection = new Connection("https://api.devnet.solana.com");
 
 // Github account
-const github = Buffer.from("testaccount", "utf8");
+const github = Buffer.from("aleynaalangil", "utf8");
 
 // Create our anchor provider
 const provider = new AnchorProvider(connection, new Wallet(keypair), { commitment: "confirmed"});
--- ts/prereqs/keygen.ts
+++ ts/prereqs/keygen.ts
@@ -7,3 +7,4 @@ console.log(`You've generated a new Solana wallet: ${kp.publicKey.toBase58()}
 To save your wallet, copy and paste the following into a JSON file:
 
 [${kp.secretKey}]`)
+// TODO: GJQAQa4nrppKYBRBrGiyXkmLF34rt6qhXfW8i4zJVQLe wallet
